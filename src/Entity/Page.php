c4e780fe11231f9cae1dbcc19f94250ac4bc57a4
Mohamed <maaouiamohamed01@gmail.com>
2024-01-16 17:12:13 +0100

    16-01-2024

---
+++ src/Entity/Page.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PageRepository::class)]
+class Page
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Auteur = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $PublicationAt = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->Auteur;
+    }
+
+    public function setAuteur(string $Auteur): static
+    {
+        $this->Auteur = $Auteur;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): static
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPublicationAt(): ?\DateTimeImmutable
+    {
+        return $this->PublicationAt;
+    }
+
+    public function setPublicationAt(\DateTimeImmutable $PublicationAt): static
+    {
+        $this->PublicationAt = $PublicationAt;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+}
